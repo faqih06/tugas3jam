f9521f3246fb607ab89f7bf2940e4001f29ebb67
faqih06 <fqhsaviar1632@gmail.com>
2019-11-26 21:59:47 +0700

    inisialisasi pertama

---
+++ DelegateEvent-master/AplikasiCalculator/frmCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AplikasiCalculator
+{
+    public partial class frmCalculator : Form
+    {
+        public delegate void OperasiHitungEventHandler(int index, int a, int b);
+
+        public event OperasiHitungEventHandler OnPerhitungan;
+
+        public frmCalculator()
+        {
+            InitializeComponent();
+            OpOperasi.Items.Add("Penjumlahan");
+            OpOperasi.Items.Add("Pengurangan");
+            OpOperasi.Items.Add("Perkalian");
+            OpOperasi.Items.Add("Pembagian");
+            OpOperasi.SelectedIndex = 0;
+            OpOperasi.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void BtnProses_Click(object sender, EventArgs e)
+        {
+            int NilaiA = int.Parse(txtNilaiA.Text);
+            int NilaiB = int.Parse(txtNilaiB.Text);
+
+            if(OpOperasi.SelectedIndex == 0)
+            {
+                OnPerhitungan(0, NilaiA, NilaiB);
+            }
+            else if(OpOperasi.SelectedIndex == 1)
+            {
+                OnPerhitungan(1, NilaiA, NilaiB);
+            }
+            else if (OpOperasi.SelectedIndex == 2)
+            {
+                OnPerhitungan(2, NilaiA, NilaiB);
+            }
+            else
+            {
+                OnPerhitungan(3, NilaiA, NilaiB);
+            }
+        }
+
+        private void cbOperasi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DelegateEvent-master/AplikasiCalculator/frmHasil.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AplikasiCalculator
+{
+    partial class frmHasil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstHasil);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(358, 308);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Hasil ]";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.ItemHeight = 25;
+            this.lstHasil.Location = new System.Drawing.Point(7, 27);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(344, 254);
+            this.lstHasil.TabIndex = 0;
+            this.lstHasil.SelectedIndexChanged += new System.EventHandler(this.lstHasil_SelectedIndexChanged);
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHitung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHitung.Location = new System.Drawing.Point(16, 331);
+            this.btnHitung.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(358, 37);
+            this.btnHitung.TabIndex = 1;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // frmHasil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 379);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmHasil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hasil Perhitungan";
+            this.Load += new System.EventHandler(this.frmHasil_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.ListBox lstHasil;
+    }
+}
+
